da08a746a672e2a3f47703f9c586bcfa1c089cc3
Dalila Destanovic <dalilad@evltn.neuro.univie.ac.at>
2023-04-14 13:23:40 +0200

    maybe better

--- cool_breakpointer_code.py
+++ cool_breakpointer_code.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 To be written
+use this code to get the scaffold names for the assemblies: grep '^>' fin_oct_vulg_30_scaff.fa | sed -E 's/^[[:space:]]*[^[:space:]]+/&,/' | sed 's/>//g' | sed 's/[^[:space:],]\+/"&"/g' | tr '\n' ' '
 """
-
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 #write the name of the query species in the alignment
 sp1 = 'Obi'
@@ -16,16 +17,13 @@ len_co= 100000
 #Write the quality cutoff for the alignments
 q = 30
 #Write what's the distance between two ends of the aligned fragments to call it a breakpoint
-len_bp = 10000000
+len_bp = 5000000
 
 #EBR=evolutionary breakpoints
 #make sure to know where you're working, can be modified later as an input
 os.chdir(r'/Users/dalilad/Desktop/The_octopus_code/breakpointer2-main')
 #os.chdir(r'/Users/dalilad/Desktop/The_octopus_code')
 #print (os.getcwd())
-
-#to disable the warning when adding the breakpoints column to the original dataframe
-pd.options.mode.chained_assignment = None
 
 #to disable the warning when adding the breakpoints column to the original dataframe
 pd.options.mode.chained_assignment = None
@@ -44,6 +42,9 @@ sp1_to_sp2= sp1_to_sp2.sort_values([sp1+'_chr',sp1+'_start',sp1+'_stop',sp2+'_ch
 #This filters the dataframe to include only relevant alignments- the length of the alignment must be higher than the values given in len_co and wuality of the alignment must be higher than value q
 sp1_to_sp2 = sp1_to_sp2[(sp1_to_sp2.alen > len_co) & (sp1_to_sp2.qual_score > q)]
 
+sp1_to_sp2 = sp1_to_sp2.reset_index()
+
+sp1_to_sp2 = sp1_to_sp2.drop(['index'], axis=1)
 #Now I want to make a dictionary of homologous chromosomes
 #since the vulg has been named according to it's homology with bimac, i can just use that, however, it would be better to have it automated and usable for wider type of datasets
 #got an idea from here: https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
@@ -51,58 +52,74 @@ dict_key=["CM046601.1", "CM046602.1", "CM046603.1", "CM046604.1", "CM046605.1", 
 dict_value=["Ovu01", "Ovu02", "Ovu03", "Ovu04", "Ovu05", "Ovu06", "Ovu07", "Ovu08", "Ovu09", "Ovu10", "Ovu11", "Ovu12", "Ovu13", "Ovu14", "Ovu15", "Ovu16", "Ovu17", "Ovu18", "Ovu19", "Ovu20", "Ovu21", "Ovu22", "Ovu23", "Ovu24", "Ovu25", "Ovu26", "Ovu27", "Ovu28", "Ovu29", "Ovu30"]
 chromosome_pairs = dict(zip(dict_key, dict_value))
 
+#.gff or .bed file part
+# 100kb windows for .bed
+#try to plot 5k thing on the HiGlass for example
+bimac_insul_sc_500kb = pd.read_csv('oct_bim_0_diff_wind_500kb.bed', delimiter='\t', header= None, names=[sp1+'_chr', sp1+'_start',sp1+'_stop', 'irrel1', 'insul_score','irrel2'])
+bimac_insul_sc_500kb = bimac_insul_sc_500kb.dropna().drop(['irrel1','irrel2'], axis=1)
+
+vulg_insul_sc_500kb = pd.read_csv('fin_oct_vulg.bed_500kb.bed', delimiter='\t', header= None, names=[sp2+'_chr', sp2+'_start',sp2+'_stop', 'irrel1', 'insul_score','irrel2'])
+vulg_insul_sc_500kb = vulg_insul_sc_500kb.dropna().drop(['irrel1','irrel2'], axis=1)
+
+pair_dataframe_fin = pd.DataFrame()
+
+#The for loop
 for k in chromosome_pairs:
     pair_dataframe=sp1_to_sp2.loc[(sp1_to_sp2[sp1+'_chr']==k) & (sp1_to_sp2[sp2+'_chr']==chromosome_pairs[k]),]
+
     #I open this list as it has been shown as the easiest-least-crashiest way to do what I want to do
     the_list = []
     #bed file starts and stops: the start is the least num and the stop is higher (regardless of the strand)
     #write an if statement and plot them vice versa for - direction
+    pair_dataframe['breakpoint_in_species'] = np.nan
+    pair_dataframe['breakpoint_in_species']=pair_dataframe['breakpoint_in_species'].astype(str)
+    #bed file starts and stops: the start is the least num and the stop is higher (regardless of the strand)
+    #write an if statement and plot them vice versa for - direction
     for i in range(len(pair_dataframe)-1):
-        row=pair_dataframe.iloc[i]
+        row = pair_dataframe.iloc[i]
         next_row=pair_dataframe.iloc[i+1]
         if next_row[sp2+'_start'] > (row[sp2+'_stop'] + len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp2
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp2
             the_list.append(row)
             the_list.append(next_row)
         if next_row[sp2+'_start'] > (row[sp2+'_start'] + len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp2
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp2
             the_list.append(row)
             the_list.append(next_row)
         if next_row[sp2+'_start'] < (row[sp2+'_stop'] - len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp2
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp2
             the_list.append(row)
             the_list.append(next_row)
         if next_row[sp2+'_stop'] > (row[sp2+'_start'] + len_bp):
-            the_list.append(row)
-            the_list.append(next_row)
-        if next_row[sp2+'_start'] > (row[sp2+'_start'] + len_bp):
-            the_list.append(row)
-            the_list.append(next_row)
-        if next_row[sp2+'_start'] > (row[sp2+'_stop'] + len_bp):
-            the_list.append(row)
-            the_list.append(next_row)
-        if next_row[sp1+'_start'] > (row[sp1+'_stop'] + len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp2
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp2
             the_list.append(row)
             the_list.append(next_row)
         if next_row[sp1+'_start'] < (row[sp1+'_stop'] - len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp1
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp1
             the_list.append(row)
             the_list.append(next_row)
-        # if next_row[sp1+'_start'] > (row[sp1+'_stop'] + len_bp):
-        #     the_list.append(row)
-        #     the_list.append(next_row)
-        # if next_row[sp1+'_start'] > (row[sp1+'_start'] + len_bp):
-        #     the_list.append(row)
-        #     the_list.append(next_row)
-        # if next_row[sp1+'_start'] < (row[sp1+'_stop'] - len_bp):
-        #     the_list.append(row)
-        #     the_list.append(next_row)
-        # if next_row[sp1+'_stop'] > (row[sp1+'_start'] + len_bp):
-        #     the_list.append(row)
-        #     the_list.append(next_row)
-        # if next_row[sp1+'_start'] > (row[sp1+'_start'] + len_bp):
-        #     the_list.append(row)
-        #     the_list.append(next_row)
-        # if next_row[sp1+'_start'] > (row[sp1+'_stop'] + len_bp):
-        #     the_list.append(row)
-        #     the_list.append(next_row)
-                
+        if next_row[sp1+'_start'] > (row[sp1+'_start'] + len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp1
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp1
+            the_list.append(row)
+            the_list.append(next_row)
+        if next_row[sp1+'_stop'] > (row[sp1+'_start'] + len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp1
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp1
+            the_list.append(row)
+            the_list.append(next_row)
+        if next_row[sp1+'_start'] > (row[sp1+'_stop'] + len_bp):
+            pair_dataframe.at[i,'breakpoint_in_species']==sp1
+            pair_dataframe.at[i+1,'breakpoint_in_species']==sp1
+            the_list.append(row)
+            the_list.append(next_row)
+    
+    #Seems like breakpoint can be detected in both, should be aware of that            
     sp1_to_sp2_final_table= pd.concat(the_list, axis=1)
     sp1_to_sp2_final_table = sp1_to_sp2_final_table.transpose().drop_duplicates()
     
@@ -133,7 +150,7 @@ for k in chromosome_pairs:
     negative_table= negative_table.transpose()
     x2_values=[negative_table[sp1+'_stop'], negative_table[sp1+'_start']]
     y2_values=[negative_table[sp2+'_start'], negative_table[sp2+'_stop']]
-    plt.plot(x1_values, y1_values, 'r', linestyle="solid")
+    plt.plot(x1_values, y1_values, 'b', linestyle="solid")
     plt.plot(x2_values, y2_values, 'b', linestyle="solid")
     # # connect adjacent points with lines
     # for i in range(len(pair_dataframe)-1):
@@ -148,20 +165,7 @@ for k in chromosome_pairs:
             breakp.append('yes')
     print(k+' vs '+chromosome_pairs[k])
     print(len(breakp)/2+1)
-        #plt.axvline(x=row[sp2+'_start'], ymin = (z-0.03), ymax = (z+0.03), color = "red", linewidth=0.5)
-# for i in range(len(plotdf)):
-#       row=plotdf.iloc[i]
-#       z=(row[sp1+'_start'])/(row[sp1+'_chr_size'])
-#       if row['breakpoints']==True:
-#           plt.axvline(x=row[sp2+'_start'], ymin = (z-0.03), ymax = (z+0.03), color = "red", linewidth=0.5)
-          
-          
-# #         #ax.stem(row[sp1+'_start'], row[sp2+'_stop'], markerfmt ='')
-# #     #normalized_data_row=(row['col4']-sp1_to_sp2_final_table['col4'].min())/(plotdf['col4'].max()-plotdf['col4'].min())
-# #     #normalized_data_row_bim=(row[sp2+'_stop']-plotdf[sp2+'_stop'].min())/(plotdf[sp2+'_stop'].max()-plotdf[sp2+'_stop'].min())
-# #     #plt.axvline(x=row[sp1+'_start'] & row['col4'], ymin = 0, ymax = row['col4'], color = "red", linewidth=0.5)
-
-    # set axis limits and labels
+    
     ax.ticklabel_format(style='plain', scilimits=(0,0))
     # formatter = ticker.ScalarFormatter(useMathText=True)
     # formatter.set_powerlimits((-3, 4))  # Set the exponent range to show
@@ -171,6 +175,20 @@ for k in chromosome_pairs:
     ax.set_title('Chromosome '+k+' vs chromosome '+chromosome_pairs[k])
     
     plt.show()
+    
+    #the .bed file part
+    #pandas.query function if this value is this or that----filters for you, doesnt iterate, dont need a for loop
+    for i in range(len(pair_dataframe)):
+        row = pair_dataframe.iloc[i]
+        if row['breakpoints']:
+            for j in range(len(bimac_insul_sc_500kb)):
+                insul_row=bimac_insul_sc_500kb.iloc[j]
+                if (insul_row[sp1+'_start'] + 5000) < row[sp1+'_stop'] and (insul_row[sp1+'_start'] - 5000) > row[sp1+'_stop']:
+                    print(insul_row['insult_score'])
+    
+    pair_dataframe_fin = pd.concat([pair_dataframe_fin, pair_dataframe])
+    
+
 
 
 
@@ -185,35 +203,4 @@ for k in chromosome_pairs:
 #plt.save can pick pdf
 #goal to have a D-Genies plot I can zoom in on
 
-#.gff or .bed file part
 
-# bimac_insul_sc_500gb= pd.read_csv('oct_bim_0_diff_wind_500kb.bed', delimiter='\t', header= None, names= [sp1+'_chr','col2',sp1+'_start','col4','relative_orientation',sp2+'_chr'])
-# test3= bimac_insul_sc_500gb.dropna()
-
-# vulg_insul_sc_500gb= pd.read_csv('fin_oct_vulg.bed_500kb.bed', delimiter='\t', header= None, names= [sp1+'_chr','col2',sp1+'_start','col4','relative_orientation',sp2+'_chr'])
-# test4= vulg_insul_sc_500gb.dropna()
-
-# # for i in range(len(sp1_to_sp2)-1):
-# #     row=sp1_to_sp2.iloc[i] 
-# #     if sp1_to_sp2.loc[i,'breakpoints'] == True:
-# #         sp1_to_sp2['place']=sp1_to_sp2[sp1+'_start'].between(test3['col2'], test3[sp1+'_start'])
-# #         if sp1_to_sp2['place'] == True:
-# #             sp1_to_sp2['insul_score']=test3['relative_orientation']
-
-# for index, row in sp1_to_sp2.iterrows():
-#     if row['breakpoints'] == True:
-#         insul_score_query = test3.query(f"{row[sp1+'_start']} >= col2 and {row[sp1+'_start']} <= col3")['relative_orientation']
-#         if not insul_score_query.empty:
-#             insul_score = insul_score_query.iloc[0]
-#             sp1_to_sp2.loc[index, 'insul_score_b'] = insul_score
-#         else:
-#             sp1_to_sp2.loc[index, 'insul_score_b'] = 'NaN'
-            
-# for index, row in sp1_to_sp2.iterrows():
-#     if row['breakpoints'] == True:
-#         insul_score_query = test4.query(f"{row[sp2+'_start']} >= col2 and {row[sp1+'_start']} <= col3")['relative_orientation']
-#         if not insul_score_query.empty:
-#             insul_score = insul_score_query.iloc[0]
-#             sp1_to_sp2.loc[index, 'insul_score_v'] = insul_score
-#         else:
-#             sp1_to_sp2.loc[index, 'insul_score_v'] = 'NaN'
