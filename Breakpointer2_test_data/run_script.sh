3c686240cd8db03c31792a1e8744651775bea7fe
Dalila Destanovic <111988103+dalila-de@users.noreply.github.com>
2024-07-31 16:46:33 -0400

    Update run_script.sh

--- Breakpointer2_test_data/run_script.sh
+++ Breakpointer2_test_data/run_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python Breakpointer2.py species1_to_species2.paf species1_500kb_window_size.bed species2_500kb_window_size.bed sp1 sp2 100000 5000000 30 100000 sp1_sp2
+python /path/to/breakpointer2/Breakpointer2.py species1_to_species2.paf species1_500kb_window_size.bed species2_500kb_window_size.bed sp1 sp2 100000 5000000 30 100000 sp1_sp2
 
 #sp1 and sp2 should be used to differengtiate species in the final results
 #100000 is the minimum length of the alignment to be processed downstream
